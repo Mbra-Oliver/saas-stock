ea66330eea991f4ba701fe83339f0eb527387aa7
Mbra Oliver <96344566+Mbra-Oliver@users.noreply.github.com>
2025-07-30 15:58:53 +0000

    Fix initial setup front

--- front-stock/app/ClientLayout.tsx
+++ front-stock/app/ClientLayout.tsx
@@ -1,8 +1,18 @@
 "use client";
+import { NiceModalProvider } from "@/components/ui/nice-modal";
 import React from "react";
+import { Toaster } from "../components/ui/sonner";
 import { ReactQueryProvider } from "../react-query/react-query-provide";
+
 function ClientLayout({ children }: { children: React.ReactNode }) {
-  return <ReactQueryProvider>{children}</ReactQueryProvider>;
+  return (
+    <ReactQueryProvider>
+      <NiceModalProvider>
+        {children}
+        <Toaster />
+      </NiceModalProvider>
+    </ReactQueryProvider>
+  );
 }
 
 export default ClientLayout;
